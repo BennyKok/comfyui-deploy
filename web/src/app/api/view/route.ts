eda922a5e3a11e0942330004e4bab075ac8e446e
BennyKok <itechbenny@gmail.com>
2023-12-21 00:50:05 +0800

    fix build

--- web/src/app/api/view/route.ts
+++ web/src/app/api/view/route.ts
@@ -1,8 +1,8 @@
-import { NextResponse, type NextRequest } from "next/server";
 import { getFileDownloadUrl } from "../../../server/getFileDownloadUrl";
+import { NextResponse, type NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
   const file = new URL(request.url).searchParams.get("file");
   if (!file) return NextResponse.redirect("/");
-  return NextResponse.redirect(getFileDownloadUrl(file));
+  return NextResponse.redirect(await getFileDownloadUrl(file));
 }
