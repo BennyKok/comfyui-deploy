fd9378b2a463a422bdf94535774a58b74debdb0f
EdwinWong <ew20128@gmail.com>
2024-01-05 14:42:11 +0800

    chore: custom node type

--- web/src/components/customInputNodes.tsx
+++ web/src/components/customInputNodes.tsx
@@ -2,4 +2,5 @@ export const customInputNodes: Record<string, string> = {
   ComfyUIDeployExternalText: "string",
   ComfyUIDeployExternalImage: "string - (public image url)",
   ComfyUIDeployExternalImageAlpha: "string - (public image url)",
+  ComfyUIDeployExternalNumber: "number"
 };
