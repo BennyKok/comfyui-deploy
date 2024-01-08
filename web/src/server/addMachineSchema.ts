b01d0374188b7227b920b47b796b3511e77c0f1c
BennyKok <itechbenny@gmail.com>
2024-01-08 12:52:30 +0800

    fix: add default values for snapshot and models

--- web/src/server/addMachineSchema.ts
+++ web/src/server/addMachineSchema.ts
@@ -11,6 +11,25 @@ export const addMachineSchema = insertMachineSchema.pick({
 export const insertCustomMachineSchema = createInsertSchema(machinesTable, {
   name: (schema) => schema.name.default("My Machine"),
   type: (schema) => schema.type.default("comfy-deploy-serverless"),
+  snapshot: (schema) =>
+    schema.snapshot.default({
+      comfyui: "d0165d819afe76bd4e6bdd710eb5f3e571b6a804",
+      git_custom_nodes: {},
+      file_custom_nodes: [],
+    }),
+  models: (schema) =>
+    schema.models.default([
+      {
+        name: "v1-5-pruned-emaonly.ckpt",
+        type: "checkpoints",
+        base: "SD1.5",
+        save_path: "default",
+        description: "Stable Diffusion 1.5 base model",
+        reference: "https://huggingface.co/runwayml/stable-diffusion-v1-5",
+        filename: "v1-5-pruned-emaonly.ckpt",
+        url: "https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main/v1-5-pruned-emaonly.ckpt",
+      },
+    ]),
 });
 
 export const addCustomMachineSchema = insertCustomMachineSchema.pick({
