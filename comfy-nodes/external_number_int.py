fa2638f66d7670c7e9d101eb36a20e9f82c9eb50
BennyKok <itechbenny@gmail.com>
2024-01-16 11:20:33 +0800

    feat(plugin): add external number input (int)

---
+++ comfy-nodes/external_number_int.py
@@ -0,0 +1,38 @@
+import folder_paths
+from PIL import Image, ImageOps
+import numpy as np
+import torch
+
+class ComfyUIDeployExternalNumberInt:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "input_id": (
+                    "STRING",
+                    {"multiline": False, "default": "input_number"},
+                ),
+            },
+            "optional": {
+                "default_value": (
+                    "INT",
+                    {"multiline": True, "display": "number", "default": 0},
+                ),
+            }
+        }
+
+    RETURN_TYPES = ("INT",)
+    RETURN_NAMES = ("value",)
+
+    FUNCTION = "run"
+
+    CATEGORY = "number"
+
+    def run(self, input_id, default_value=None):
+        if not input_id or len(input_id.strip()) == 0:
+            return [default_value]
+        return [input_id]
+
+
+NODE_CLASS_MAPPINGS = {"ComfyUIDeployExternalNumberInt": ComfyUIDeployExternalNumberInt}
+NODE_DISPLAY_NAME_MAPPINGS = {"ComfyUIDeployExternalNumberInt": "External Number Int (ComfyUI Deploy)"}
