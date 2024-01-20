a2d0e931720e8b9b9fe115e8958215164f080ff6
Nicholas Koben Kao <kobenkao@gmail.com>
2024-01-19 19:38:35 -0800

    posthog install. from: https://posthog.com/docs/libraries/next-js

--- web/src/app/(app)/layout.tsx
+++ web/src/app/(app)/layout.tsx
@@ -7,6 +7,13 @@ import meta from "next-gen/config";
 import PlausibleProvider from "next-plausible";
 import { Inter } from "next/font/google";
 import { Toaster } from "sonner";
+import { PHProvider } from "./providers";
+
+import dynamic from "next/dynamic";
+
+const PostHogPageView = dynamic(() => import("./PostHogPageView"), {
+  ssr: false,
+});
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -39,20 +46,23 @@ export default function RootLayout({
               <PlausibleProvider domain={process.env.PLAUSIBLE_DOMAIN} />
             </head>
           )}
-          <body className={inter.className}>
-            <main className="w-full flex min-h-[100dvh] flex-col items-center justify-start">
-              <div className="z-[-1] fixed h-full w-full bg-white">
-                <div className="absolute h-full w-full bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_70%,transparent_100%)]" />
-              </div>
-              <div className="sticky w-full h-18 flex items-center justify-between gap-4 p-4 border-b border-gray-200">
-                <Navbar />
-              </div>
-              <div className="md:px-10 px-6 w-full h-[calc(100dvh-73px)]">
-                {children}
-              </div>
-              <Toaster richColors />
-            </main>
-          </body>
+          <PHProvider>
+            <body className={inter.className}>
+              <PostHogPageView />
+              <main className="w-full flex min-h-[100dvh] flex-col items-center justify-start">
+                <div className="z-[-1] fixed h-full w-full bg-white">
+                  <div className="absolute h-full w-full bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_70%,transparent_100%)]" />
+                </div>
+                <div className="sticky w-full h-18 flex items-center justify-between gap-4 p-4 border-b border-gray-200">
+                  <Navbar />
+                </div>
+                <div className="md:px-10 px-6 w-full h-[calc(100dvh-73px)]">
+                  {children}
+                </div>
+                <Toaster richColors />
+              </main>
+            </body>
+          </PHProvider>
         </TooltipProvider>
       </ClerkProvider>
     </html>
