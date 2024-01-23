6ed7807081df58550c4dd009d20869cb88b8ab44
BennyKok <itechbenny@gmail.com>
2024-01-23 10:43:53 +0800

    bump default machine comfy deploy version

--- web/src/server/addMachineSchema.ts
+++ web/src/server/addMachineSchema.ts
@@ -17,7 +17,7 @@ export const insertCustomMachineSchema = createInsertSchema(machinesTable, {
       comfyui: "d0165d819afe76bd4e6bdd710eb5f3e571b6a804",
       git_custom_nodes: {
         "https://github.com/BennyKok/comfyui-deploy.git": {
-          hash: "1e99474780951043e1b001e1877de3f2eb45cfba",
+          hash: "43fe0a384aa5fa9e141d4a264b2ed40a73b817bc",
           disabled: false,
         },
       },
