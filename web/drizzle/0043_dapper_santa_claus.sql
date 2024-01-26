e95811474ef414fd70c7d155bb6873421b960bd8
bennykok <itechbenny@gmail.com>
2024-01-26 11:58:16 +0800

    fix: migration again

--- web/drizzle/0043_dapper_santa_claus.sql
+++
@@ -1,7 +0,0 @@
-DO $$ BEGIN
- CREATE TYPE "model_type" AS ENUM('checkpoint', 'lora', 'embedding', 'vae');
-EXCEPTION
- WHEN duplicate_object THEN null;
-END $$;
---> statement-breakpoint
-ALTER TABLE "comfyui_deploy"."checkpoints" ADD COLUMN "model_type" "model_type" NOT NULL;
--- web/drizzle/0043_wealthy_nicolaos.sql
+++ web/drizzle/0043_wealthy_nicolaos.sql
@@ -1,3 +1,9 @@
+DO $$ BEGIN
+ CREATE TYPE "model_type" AS ENUM('checkpoint', 'lora', 'embedding', 'vae');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
 ALTER TABLE "comfyui_deploy"."checkpoints" RENAME TO "models";--> statement-breakpoint
 ALTER TABLE "comfyui_deploy"."checkpoint_volume" RENAME TO "user_volume";--> statement-breakpoint
 ALTER TABLE "comfyui_deploy"."models" RENAME COLUMN "checkpoint_volume_id" TO "user_volume_id";--> statement-breakpoint
@@ -7,7 +13,7 @@ ALTER TABLE "comfyui_deploy"."models" DROP CONSTRAINT "checkpoints_checkpoint_vo
 --> statement-breakpoint
 ALTER TABLE "comfyui_deploy"."user_volume" DROP CONSTRAINT "checkpoint_volume_user_id_users_id_fk";
 --> statement-breakpoint
-ALTER TABLE "comfyui_deploy"."models" ALTER COLUMN "model_type" SET DEFAULT 'checkpoint';--> statement-breakpoint
+ALTER TABLE "comfyui_deploy"."models" ADD COLUMN "model_type" "model_type" DEFAULT 'checkpoint' NOT NULL;--> statement-breakpoint
 DO $$ BEGIN
  ALTER TABLE "comfyui_deploy"."models" ADD CONSTRAINT "models_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "comfyui_deploy"."users"("id") ON DELETE no action ON UPDATE no action;
 EXCEPTION
